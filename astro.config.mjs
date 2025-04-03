4219b4275a744c99547b3570811443f573b0b003
adan sanchez hernandez <adansaherr30@gmail.com>
2025-04-02 23:10:55 -0600

    Retry Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://adan0930.github.io/AS-Portfolio/',  // Cambia por tu URL de GitHub Pages
   output: 'static', // Establece la salida como estática
-  base:'/As-Portfolio/',  
-  build: {
-    outDir: './docs',  // La carpeta donde se generarán los archivos estáticos
-    format:'directory',
-  },
 });
  
 
