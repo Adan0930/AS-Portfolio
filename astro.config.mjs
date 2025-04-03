f7d1de923b250c3fa3ffacd52890efaa321b96ec
Adan Sanchez <adansaherr30@gmail.com>
2025-04-03 13:32:59 -0600

    fix(config.astro):dir-dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://adan0930.github.io/AS-Portfolio/',  // Cambia por tu URL de GitHub Pages
-  output: 'static', // Establece la salida como estática
+  site: 'https://adan0930.github.io/AS-Portfolio/',  // URL de GitHub Pages
+  build: {
+    outDir: 'dist',  // Carpeta donde se generarán los archivos estáticos
+  },
 });
- 
-
 
 
